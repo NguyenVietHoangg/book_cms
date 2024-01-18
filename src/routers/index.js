df7c4f37c3feeef94916678ebeb6bcc3033ddfdf
NguyenVietHoangg <nvhoangtb2k2@gmail.com>
2024-01-18 15:40:45 +0700

    a

--- src/routers/index.js
+++ src/routers/index.js
@@ -26,48 +26,56 @@ const routes = [
   {
     path: "/app/product/:id",
     name: "Product Detail",
+    beforeEnter: requireAuth,
     component: () => import("@/pages/Product/ProductDetail.vue"),
     props: {},
   },
   {
     path: "/app/category",
     name: "Category List",
+    beforeEnter: requireAuth,
     component: () => import("@/pages/Category/CategoryList.vue"),
     props: {},
   },
   {
     path: "/app/collection/:id",
     name: "Collection Detail",
+    beforeEnter: requireAuth,
     component: () => import("@/pages/Collection/CollectionDetail.vue"),
     props: {},
   },
   {
     path: "/app/collection",
     name: "Collection List",
+    beforeEnter: requireAuth,
     component: () => import("@/pages/Collection/CollectionList.vue"),
     props: {},
   },
   {
     path: "/app/category/:id",
     name: "Category Detail",
+    beforeEnter: requireAuth,
     component: () => import("@/pages/Category/CategoryDetail.vue"),
     props: {},
   },
   {
     path: "/app/media",
     name: "Media List",
+    beforeEnter: requireAuth,
     component: () => import("@/pages/Media/MediaList.vue"),
     props: {},
   },
   {
     path: "/app/setting-general",
     name: "Setting General",
+    beforeEnter: requireAuth,
     component: () => import("@/pages/Setting/SettingGeneral.vue"),
     props: {},
   },
   {
     path: "/app/menu",
     name: "Menu List",
+    beforeEnter: requireAuth,
     component: () => import ('@/pages/Menu/MenuList.vue'),
     props: {
     }
@@ -75,12 +83,14 @@ const routes = [
   {
     path: "/app/permission",
     name: "Setting Permission",
+    beforeEnter: requireAuth,
     component: () => import ('@/pages/Setting/Permission.vue'),
     props: {
     }
   },
   {
     path: "/:catchAll(.*)",
+    beforeEnter: requireAuth,
     name: "Exception",
     component: () => import("@/pages/Exception/Error.vue"),
   },
